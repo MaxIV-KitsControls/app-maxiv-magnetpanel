8f15c336189244da8425827a4139393fd3233e3a
Johan Forsberg <johan.forsberg@maxlab.lu.se>
2014-10-09 09:37:47 +0200

    packaging

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="python-magnetpanel",
+    version="0.2.0",
+    description="QT widget for controlling a magnet circuit.",
+    author="Johan Forsberg",
+    author_email="johan.forsberg@maxlab.lu.se",
+    license="GPLv3",
+    url="http://www.maxlab.lu.se",
+    include_package_data=True,
+    packages=find_packages(),
+    scripts=["scripts/ctmagnet"]
+)
