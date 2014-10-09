98d77625b2147504cc5dbcdff5344e9801aeaf25
Johan Forsberg <johan.forsberg@maxlab.lu.se>
2014-10-09 16:57:50 +0200

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="python-magnetpanel",
-    version="0.2.0",
+    version="0.3.0",
     description="QT widget for controlling a magnet circuit.",
     author="Johan Forsberg",
     author_email="johan.forsberg@maxlab.lu.se",
