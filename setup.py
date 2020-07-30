2660d0336db8c66f14f25debee9828b8601221eb
Wojciech Kitka <wojciech.kitka@s2innovation.com>
2020-07-30 09:40:49 +0200

    Bump version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="python-magnetpanel",
-    version="0.7.2",
+    version="0.7.3",
     description="QT widget for controlling a magnet circuit.",
     author="Johan Forsberg",
     author_email="johan.forsberg@maxlab.lu.se",
